7ba36259d67b349d22d886ed63bd18efc2a356d9
luischo04 <83773720+luischo04@users.noreply.github.com>
2022-03-19 22:56:54 -0600

    archivo conexion
    Archivo de conexión establecido y listo para el uso dentro del proyecto.

---
+++ config/conexion.php
@@ -0,0 +1,36 @@
+<?php
+    session_start();
+
+    class Conectar{
+
+        protected $dbh;
+
+        /**
+         * Funcion que conecta con la base de datos
+         */
+        protected function conexion() {
+            try {
+                $conectar = $this->dbh = new PDO("mysql:local=localhost; dbname=control_acceso","root","");
+                return $conectar;
+            } catch (Exception $e) {
+                print "Error BD: " . $e->getMessage() . "</br>";
+                die();
+            }
+        }
+
+        /**
+         * Funcion que resplada en caso de problemas con simbolos ortograficos
+         */
+        public function set_names() {
+            return $this->dbh->query("SET NAMES 'utf-8'");
+        }
+
+        /**
+         * Funcion que redireccion a login o pantalla inicial segun sea el caso.
+         */
+        public static function ruta() {
+            return "http://localhost/ControlAcceso/";
+        }
+
+    }
+?>
